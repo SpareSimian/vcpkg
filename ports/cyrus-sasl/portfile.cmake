2701855b4e1898fb1861c480c9dd0b8c66a208f4
Kenneth Porter <SpareSimian+github@SewingWitch.com>
2019-06-13 04:31:36 -0700

    cyrus-sasl now installs without errors.

--- ports/cyrus-sasl/portfile.cmake
+++ ports/cyrus-sasl/portfile.cmake
@@ -6,11 +6,16 @@ vcpkg_from_github(
     REF cyrus-sasl-2.1.27
     SHA512 2138d4b122da5a326ad94c6c9f6cb0e959f87c2ed991f926afd22a5d4b390ef0380aece689ff48ceb4c0f0ae305a7472fd5b8fe905f800d04a4f0d4e16248644
     HEAD_REF master
-    PATCHES AdditionalDirectories.patch
+    PATCHES AdditionalDirectories.patch lmdb-debug.patch
 )
 
-
-file(TO_NATIVE_PATH ${CURRENT_PACKAGES_DIR} CURRENT_PACKAGES_DIR_NATIVE)
+# copy the six headers to our install directory
+file(COPY ${SOURCE_PATH}/include/hmac-md5.h DESTINATION ${CURRENT_PACKAGES_DIR}/include/sasl)
+file(COPY ${SOURCE_PATH}/include/md5.h DESTINATION ${CURRENT_PACKAGES_DIR}/include/sasl)
+file(COPY ${SOURCE_PATH}/include/sasl.h DESTINATION ${CURRENT_PACKAGES_DIR}/include/sasl)
+file(COPY ${SOURCE_PATH}/include/saslplug.h DESTINATION ${CURRENT_PACKAGES_DIR}/include/sasl)
+file(COPY ${SOURCE_PATH}/include/saslutil.h DESTINATION ${CURRENT_PACKAGES_DIR}/include/sasl)
+file(COPY ${SOURCE_PATH}/include/prop.h DESTINATION ${CURRENT_PACKAGES_DIR}/include/sasl)
 
 # update ALL the projects to the latest build tool
 vcpkg_execute_required_process(
@@ -20,6 +25,11 @@ vcpkg_execute_required_process(
     WORKING_DIRECTORY ${SOURCE_PATH}/win32
     LOGNAME upgrade-${TARGET_TRIPLET}
 )
+
+# cyrus-sasl-all-in-one would also build plugin_gssapiv2 but that
+# requires a krb library so we build all the other projects
+# individually. We also don't want the install project as we do that
+# directly with cmake/vcpkg operations.
 
 vcpkg_install_msbuild(
     SOURCE_PATH ${SOURCE_PATH}
@@ -101,56 +111,3 @@ vcpkg_install_msbuild(
     REMOVE_ROOT_INCLUDES
     USE_VCPKG_INTEGRATION
 )
-
-# no gssapi package in vcpkg
-if(0)
-vcpkg_install_msbuild(
-    SOURCE_PATH ${SOURCE_PATH}
-    PROJECT_SUBPATH win32/plugin_gssapiv2.vcxproj
-    INCLUDES_SUBPATH include
-    LICENSE_SUBPATH COPYING
-    REMOVE_ROOT_INCLUDES
-    USE_VCPKG_INTEGRATION
-)
-endif()
-
-vcpkg_install_msbuild(
-    SOURCE_PATH ${SOURCE_PATH}
-    PROJECT_SUBPATH win32/install.vcxproj
-    INCLUDES_SUBPATH include
-    LICENSE_SUBPATH COPYING
-    REMOVE_ROOT_INCLUDES
-    USE_VCPKG_INTEGRATION
-    OPTIONS "/p:InstallRoot=${CURRENT_PACKAGES_DIR_NATIVE}"
-)
-
-if(0)
-#set(CYRUS_SOLUTION "cyrus-sasl-core.sln")
-#set(CYRUS_SOLUTION "cyrus-sasl-all-in-one.sln")
-if(VCPKG_TARGET_ARCHITECTURE STREQUAL x86)
-    # Projects use x86 instead of Win32
-    vcpkg_install_msbuild(
-        PLATFORM x86
-        SOURCE_PATH ${SOURCE_PATH}
-        PROJECT_SUBPATH win32/${CYRUS_SOLUTION}
-        INCLUDES_SUBPATH include
-        LICENSE_SUBPATH COPYING
-        REMOVE_ROOT_INCLUDES
-        USE_VCPKG_INTEGRATION
-        OPTIONS "/p:InstallRoot=${CURRENT_PACKAGES_DIR_NATIVE}"
-    )
-else()
-    vcpkg_install_msbuild(
-        SOURCE_PATH ${SOURCE_PATH}
-        PROJECT_SUBPATH win32/${CYRUS_SOLUTION}
-        INCLUDES_SUBPATH include
-        LICENSE_SUBPATH COPYING
-        REMOVE_ROOT_INCLUDES
-        USE_VCPKG_INTEGRATION
-        OPTIONS "/p:InstallRoot=${CURRENT_PACKAGES_DIR_NATIVE}"
-    )
-endif()
-endif()
-
-# core only solution doesn't build plugins
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/plugins)
